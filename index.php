c3c0ee4fed50d4ba222a882f86bdac27aea821c7
donald <donald@w.mediaplanetus.com>
2026-04-08 22:49:20 +0600

    fix: update debug statement in index.php

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
 
 use Illuminate\Foundation\Application;
 use Illuminate\Http\Request;
-die('ssssssssss');
+
 define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
-
+die('dddddddd');
 // Register the Composer autoloader...
 require __DIR__.'vendor/autoload.php';
 
